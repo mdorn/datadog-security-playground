10497f0eec5c4544e6df6c68b2dc19724029a843
Lorenzo Susini <lorenzo.susini@datadoghq.com>
2026-02-09 13:33:23 +0100

    update: use medium aws instances

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -126,17 +126,17 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 2
+      desired_size = 1
     }
 
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 2
